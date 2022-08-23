365838b81bbc0f51b085ecea2c0badb4dc8d588f
garryod <garry.euan.odonnell@ntlworld.com>
2022-08-23 23:36:18 +0100

    Increase terrain size

--- src/terrain.rs
+++ src/terrain.rs
@@ -9,7 +9,7 @@ impl TerrainPlugin {
         mut standard_material_assets: ResMut<Assets<StandardMaterial>>,
     ) {
         commands.spawn_bundle(PbrBundle {
-            mesh: mesh_assets.add(Mesh::from(Plane { size: 100.0 })),
+            mesh: mesh_assets.add(Mesh::from(Plane { size: 120.0 })),
             material: standard_material_assets
                 .add(StandardMaterial::from(Color::hsl(135.0, 0.5, 0.25))),
             ..default()
