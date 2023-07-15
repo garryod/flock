181f4f6465a00918919de39982f78c25e60925e5
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2023-07-15 14:44:39 +0100

    Simplify wasm launcher

--- launchers/wasm/src/main.rs
+++ launchers/wasm/src/main.rs
@@ -1,8 +1,4 @@
 use bevy::prelude::*;
-use stylist::yew::styled_component;
-use stylist::{css, global_style};
-use yew::prelude::*;
-
 use my_game::LAUNCHER_TITLE;
 
 fn set_window_title(title: &str) {
@@ -12,48 +8,9 @@ fn set_window_title(title: &str) {
         .set_title(title);
 }
 
-fn set_global_css() {
-    global_style! {
-        r#"
-        html {
-            min-height: 100%;
-            position: relative;
-        }
-        body {
-            height: 100%;
-            padding: 0;
-            margin: 0;
-        }
-        "#
-    }
-    .expect("Unable to mount global style");
-}
-
-#[styled_component(Root)]
-fn view() -> Html {
-    set_window_title(LAUNCHER_TITLE);
-    set_global_css();
-
-    let css = css!(
-        r#"
-        position: absolute;
-        overflow: hidden;
-        width: 100%;
-        height: 100%;
-        "#
-    );
-
-    html! {
-        <div class={ css }>
-            <canvas id="bevy"></canvas>
-        </div>
-    }
-}
-
 fn main() {
-    // Mount the DOM
-    yew::Renderer::<Root>::new().render();
     // Start the Bevy App
+    set_window_title(LAUNCHER_TITLE);
     let mut app = my_game::app();
     info!("Starting launcher: WASM");
     app.run();
